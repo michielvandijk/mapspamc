e059c02d45c8b37de284f4dd95d83c760915add4
michielvandijk <michiel.vandijk@wur.nl>
2021-11-30 23:05:28 +0100

    removed fao code from print

--- R/print.R
+++ R/print.R
@@ -3,7 +3,6 @@ print.spamc_par <- function(x, ...) {
     cat("country name: ", x$country, "\n")
     cat("iso3n: ", x$iso3n, "\n")
     cat("iso3c: ", x$iso3c, "\n")
-    cat("fao code: ", x$fao_code, "\n")
     cat("continent: ", x$continent, "\n")
     cat("year: ", x$year, "\n")
     cat("model: ", x$model, "\n")
