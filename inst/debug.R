494cd4c994a34ac48253f64c92c2172ba1054422
michielvandijk <michiel.vandijk@wur.nl>
2022-09-02 14:51:09 +0200

    add replacement in gaez_update for bana

--- inst/debug.R
+++ inst/debug.R
@@ -16,7 +16,7 @@ if(!require(pacman)) install.packages("pacman")
 library(pacman)
 
 # Load required packages
-p_load(mapspamc, countrycode, here, glue, terra, readxl, tidyverse, sf, ggpubr, viridis)
+p_load(mapspamc, countrycode, here, glue, terra, readxl, tidyverse, sf, ggpubr, viridis, tictoc)
 
 # R options
 options(scipen=999) # Suppress scientific notation
@@ -24,35 +24,43 @@ options(digits=4) # limit display to four digits
 
 
 # SETUP MAPSPAMC -------------------------------------------------------------------------
+# Set the folders where the scripts, model and database will be stored.
+# Note that R uses forward slashes even in Windows!!
+
 # Creates a model folder structure in c:/temp/ with the name 'mapspamc_mwi'.
 # the user can replace mwi with the country code of the case-study country or
 # choose a new name
-model_path <- "C:/Users/dijk158/OneDrive - Wageningen University & Research/data/mapspamc_iso3c/mapspamc_mwi"
-#model_path <- "c:/temp/mapspamc_mwi"
+model_path <- "C:/Users/dijk158/OneDrive - Wageningen University & Research/data/mapspamc_iso3c/mapspamc_eth"
 
 # Creates a database folder with the name mapspamc_db in c:/temp
 db_path <- "c:/temp"
 
 # Sets the location of the version of GAMS that will be used to solve the model
 gams_path <- "C:/MyPrograms/GAMS/win64/24.6"
-gams_path <- "C:/MyPrograms/GAMS/win64/24.1"
-
-gams <- gdxrrw::igdx(param$gams_path, returnStr=FALSE)
-gams
-gams == TRUE
-gams <- gdxrrw::igdx()
-isTRUE(gams)
 
 # Set mapspamc parameters for the min_entropy_5min_adm_level_2_solve_level_0 model
+# param <- mapspamc_par(
+#   model_path = model_path,
+#   db_path = db_path,
+#   gams_path = gams_path,
+#   iso3c = "ETH",
+#   year = 2015,
+#   res = "5min",
+#   adm_level = 2,
+#   solve_level = 1,
+#   model = "min_entropy")
+
+# # Set mapspamc parameters for the max_score_30sec_adm_level_2_solve_level_0 model
 param <- mapspamc_par(
   model_path = model_path,
   db_path = db_path,
   gams_path = gams_path,
-  iso3c = "MWI",
-  year = 2010,
-  res = "5min",
+  iso3c = "ETH",
+  year = 2015,
+  res = "30sec",
   adm_level = 2,
-  solve_level = 0,
-  model = "min_entropy")
+  solve_level = 1,
+  model = "max_score")
+
 
 
